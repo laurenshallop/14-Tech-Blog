3b3fc479eccb774ca1f937f84c60765b11a82159
Lauren Lucio <laurenlucio@Laurens-MacBook-Pro.local>
2022-05-21 16:16:25 -0700

    added in POST file

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+// had to capitalize 'P'
+
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+// define table columns and configuration
+Post.init(
+    {
+      // define an id column
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len:[1]
+        }
+      },
+      post_text: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+            len:[1]
+            }
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: 'user',
+            key: 'id'
+            }
+        }
+    },
+{
+
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post'
+  }
+);
+
+module.exports = Post;
